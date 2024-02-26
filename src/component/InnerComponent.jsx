db776f07b7ef27c28122b34affaf54a15aaa9167
pushpaksharma <sharmatryhard@gmail.com>
2024-02-25 22:57:03 -0500

    Event Listner added

--- src/component/InnerComponent.jsx
+++ src/component/InnerComponent.jsx
@@ -1,17 +1,26 @@
+// Import React and useMemo hook from react module
 import React, { useMemo } from 'react';
 
+// Import large-file.json data
 import data from "../file/large-file.json";
 
-// heavy computational task
+// Heavy computational task to get the length of data
 function getDataLength(data) {
     console.log("Rendering Heavy Computational Task")
     let count = 0;
-    for( let i = 0; i < data.length; i++ ) count++;
+    for (let i = 0; i < data.length; i++) count++;
     return count;
+}
+
+// Event handler function for datalog loaded event
+function handleDatalog() {
+    console.log('Datalog loaded');
 }
 
 // Inner component
 const InnerComponent = (props) => {
+    // Attach event listener for datalog loaded event
+    window.addEventListener('load', handleDatalog);
     
     // Memoize the heavy computation to avoid re-executing it on every render
     const totalData = useMemo(() => getDataLength(data), [] );
@@ -23,5 +32,5 @@ const InnerComponent = (props) => {
         </div>
     );
 };
-  
+
 export default InnerComponent;
